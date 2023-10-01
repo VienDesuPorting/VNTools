4b76a600fb659c5a85d5b097a279db6fb4c7b3ca
OleSTEEP <krinkinoleg@gmail.com>
2023-10-01 22:56:28 +0300

    Add installing Nuitka in build script

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir output
 mkdir output/bin
+python3 -m pip install Nuitka
 nuitka3 --jobs=$(nproc) --output-dir=output --follow-imports --output-filename=output/bin/ffmpeg-comp FFMpeg-Compressor/main.py
 cp FFMpeg-Compressor/ffmpeg-comp.toml output/bin/
 nuitka3 --jobs=$(nproc) --output-dir=output --follow-imports --output-filename=output/bin/rendroid-unpack RenPy-Android-Unpack/unpack.py
