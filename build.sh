e9777b8779ad6208da8ec0091964760b07073936
OleSTEEP <krinkinoleg@gmail.com>
2023-10-07 01:01:11 +0300

    Determining logic cpu count in Darwin

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 mkdir output
 mkdir output/bin
 python3 -m pip install Nuitka
-nuitka3 --jobs=$(nproc) --output-dir=output --follow-imports --output-filename=output/bin/ffmpeg-comp FFMpeg-Compressor/main.py
+case "$(uname -s)" in
+    Linux*)     jobs="--jobs=$(nproc)";;
+    Darwin*)    jobs="--jobs=$(sysctl -n hw.ncpu)";;
+esac
+nuitka3 "${jobs}" --output-dir=output --follow-imports --output-filename=output/bin/ffmpeg-comp FFMpeg-Compressor/main.py
 cp FFMpeg-Compressor/ffmpeg-comp.toml output/bin/
-nuitka3 --jobs=$(nproc) --output-dir=output --follow-imports --output-filename=output/bin/rendroid-unpack RenPy-Android-Unpack/unpack.py
+nuitka3 "${jobs}" --output-dir=output --follow-imports --output-filename=output/bin/rendroid-unpack RenPy-Android-Unpack/unpack.py
