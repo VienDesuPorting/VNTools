dc858b4ebbce1fbcdb6c995f1a7b303435036c25
OleSTEEP <krinkinoleg@gmail.com>
2024-01-12 23:12:19 +0300

    Overwrite binaries in output folder

--- build.bat
+++ build.bat
@@ -4,11 +4,10 @@ mkdir output\bin
 python -m pip install -r FFMpeg-Compressor\requirements.txt || goto :exit
 python -m pip install Nuitka || goto :exit
 python -m nuitka --jobs=%NUMBER_OF_PROCESSORS% --output-dir=output --follow-imports --onefile --output-filename=ffmpeg-comp FFMpeg-Compressor\main.py || goto :exit
-xcopy FFMpeg-Compressor\ffmpeg-comp.toml output\bin
-move output\ffmpeg-comp.exe output\bin
+xcopy FFMpeg-Compressor\ffmpeg-comp.toml output\bin /Y
+move /Y output\ffmpeg-comp.exe output\bin
 python -m nuitka --jobs=%NUMBER_OF_PROCESSORS% --output-dir=output --follow-imports --onefile --output-filename=rendroid-unpack RenPy-Android-Unpack\unpack.py || goto :exit
-move output\rendroid-unpack.exe output\bin
-pause
+move /Y output\rendroid-unpack.exe output\bin
 
 :exit
 pause
