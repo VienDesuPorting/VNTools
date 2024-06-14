da64641bedcbe1f5563ae8bbeac2b532441b40d1
OleSTEEP <krinkinoleg@gmail.com>
2024-06-15 02:56:27 +0300

    FFMpeg-Compressor: Do not convert not transparent to RGBA

--- FFMpeg-Compressor/modules/compressor.py
+++ FFMpeg-Compressor/modules/compressor.py
@@ -87,13 +87,16 @@ def compress_image(folder, file, target_folder, extension):
     quality = configloader.config['IMAGE']['Quality']
     printer.files(file, os.path.splitext(file)[0], extension, f"{quality}%")
     try:
-        image = Image.open(f'{folder}/{file}').convert('RGBA')
+        image = Image.open(f'{folder}/{file}')
 
         if (extension == "jpg" or extension == "jpeg" or
                 (extension == "webp" and not configloader.config['FFMPEG']['WebpRGBA'])):
             if has_transparency(image):
                 printer.warning(f"{file} has transparency. Changing to fallback...")
                 extension = configloader.config['IMAGE']['FallBackExtension']
+
+        if has_transparency(image):
+            image.convert('RGBA')
 
         res_downscale = configloader.config['IMAGE']['ResDownScale']
         if res_downscale != 1:
